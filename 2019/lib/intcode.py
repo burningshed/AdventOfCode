423d25d3e37916bbfddfd19f4b655f9aadf24800
burningshed <burningshed@gmail.com>
2019-12-14 10:01:24 -0500

    intocde works for 1, 2, 99 again with an extendable framework now

--- 2019/lib/intcode.py
+++ 2019/lib/intcode.py
@@ -8,69 +8,8 @@ note: writing the dictionary out would have been clearer actually
 """
 
 # TODO: figure out how to do this
-OP_DICT = {
-    1: intcode_add,
-    2: intcode_mult,
-    3: intcode_get_input,
-    4: intcode_get_output
-    99: intcode_quit
-}
 
 
-def intcode_add(code, pointer, argstring=(0, 0, 0), data_stream=None):
-    """
-    moves pointer 4 positions forward, adds positions 1 and 2,
-    outputs position 3
-    """
-    in1 = code[pointer+1]
-    in2 = code[pointer+2]
-    if not argstring[0]:
-        in1 = code[in1]
-    if not argstring[1]:
-        in2 = code[in2]
-    result = in1 + in2
-    if not argstring[2]:
-        code[pointer+2] = result
-    else:
-        out1 = code[pointer+2]
-        code[out1] = result
-    status = 1
-    return code, status
-
-
-def intcode_mult(code, pointer, argstring=(0, 0, 0), data_stream=None):
-    """
-    moves pointer 4 positions forward, adds positions 1 and 2,
-    outputs position 3
-    """
-    in1 = code[pointer+1]
-    in2 = code[pointer+2]
-    if not argstring[0]:
-        in1 = code[in1]
-    if not argstring[1]:
-        in2 = code[in2]
-    result = in1 * in2
-    if not argstring[2]:
-        code[pointer+2] = result
-    else:
-        out1 = code[pointer+2]
-        code[out1] = result
-    status = 1
-    return code, status
-
-
-def intcode_get_input(code, pointer, argstring):
-    status = 1
-    return code, status
-
-
-def intcode_get_output(code, pointer, argstring):
-    status = 1
-    return code, status
-
-
-def intcode_exit(code, pointer, argstring, data_stream):
-    return code, 0
 
 
 
@@ -86,30 +25,88 @@ class int_computer():
         """
         self.code = init_code
         self.pointer = 0
+        self.OP_DICT = {
+            1: self.intcode_add,
+            2: self.intcode_mult,
+            3: self.intcode_get_input,
+            4: self.intcode_get_output,
+            99: self.intcode_quit
+        }
+ 
+    def intcode_add(self, argstring=(0, 0, 0)):
+        """
+        moves pointer 4 positions forward, adds positions 1 and 2,
+        outputs position 3
+        """
+        STEP_SIZE = 4
+        in1 = int(self.code[self.pointer+1])
+        in2 = int(self.code[self.pointer+2])
+        if not argstring[0]:
+            in1 = int(self.code[in1])
+        if not argstring[1]:
+            in2 = int(self.code[in2])
+            result = in1 + in2
+        if not argstring[2]:
+            out1 = int(self.code[self.pointer+2])
+            self.code[out1] = str(result)
+        else:
+            self.code[self.pointer+2] = str(result)
+        self.pointer += STEP_SIZE
+        return 1
 
+    def intcode_mult(self, argstring):
+        """
+        moves pointer 4 positions forward, multiplies positions 1 and 2,
+        outputs position 3
+        """
+        STEP_SIZE = 4
+        in1 = int(self.code[self.pointer+1])
+        in2 = int(self.code[self.pointer+2])
+        if not argstring[0]:
+            in1 = int(self.code[in1])
+        if not argstring[1]:
+            in2 = int(self.code[in2])
+            result = in1 * in2
+        if not argstring[2]:
+            out1 = int(self.code[self.pointer+2])
+            self.code[out1] = str(result)
+        else:
+            self.code[self.pointer+2] = str(result)
+        self.pointer += STEP_SIZE
+        return 1
+
+    def intcode_get_input(self, argstring):
+        status = 1
+        return status
+
+    def intcode_get_output(self, argstring):
+        status = 1
+        return status
+
+    def intcode_quit(self, argstring):
+        return 0
+
+   
     def run_step(self):
         """
         perform one operation,
         """
         instruction_str = self.code[self.pointer]
         opcode = instruction_str[-2:]
-        # intcode operations format:
-        # intcode_op(code, pointer, argstring, datastream)
-        self.code, status = OP_DICT[opcode](self.code, self.pointer,
-                                            argstring, datastream)
+        # define argstring here
+        # default
+        argstring = (0, 0, 0)
+        status = self.OP_DICT[int(opcode)](argstring)
+        return status
 
-    def run_code(self, code=None):
+    def run_code(self):
         """
         run through intcode, if no code is provided use the code provided when class was created
         returns 0 if 99 was reached, 2 if some unexpected op-code encountered
         """
-        if code is None:
-            code = self.code
         retVal = 1
-        curLoc = 0
         while(retVal == 1):
-            retVal = self.run_step(curLoc, code)
-            curLoc += 4
+            retVal = self.run_step()
         return retVal
 
     def get_code(self):
@@ -117,9 +114,10 @@ class int_computer():
 
 
 if __name__ == "__main__":
-    testCase = int_computer(
+    testCase2 = int_computer(
         ["1", '9', '10', '3', '2', '3', '11', '0', '99', '30', '40', '50'])
-    #print(testCase.get_code())
-    #print(testCase.run_code())
-    #print(testCase.get_code())
-    testCase.run_step()
+    testCase1 = int_computer(["1", "1", "1", "1", "99"])
+    print(testCase1.get_code())
+    print(testCase1.run_code())
+    print(testCase1.get_code())
+    #testCase.run_step()
