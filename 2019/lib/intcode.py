4982c89a3890186abf1ae8dc8b83cc58aaabcf0f
Robert Woehrle <burningshed@gmail.com>
2019-12-22 00:28:02 -0500

    rewriting int_computer lib - still writing parser

--- 2019/lib/intcode.py
+++ 2019/lib/intcode.py
@@ -23,199 +23,72 @@ class fixed_input:
             self.pointer = 0
         return entry
 
-
-
-
-
-
-
-
 class int_computer():
     """
     operates on intcodes per Advent of Code 2019 - Day 2
     """
-    def __init__(self, init_code,
-                 input_stream=None, output_stream=print,
-                 debug=False):
-        """
-        Initialize with some intcode
-        """
-        self.output_stream=output_stream
-        self.debug = debug
-
-        self.input_stream=input_stream
-        self.code = init_code
+    OP_KEY = {
+        1: (self.debug, 4),
+        2: (self.debug, 4),
+        3: (self.debug, 2),
+        4: (self.debug, 2),
+        88: (self.debug, 1),
+        99: (self.terminate, 0)
+    }
+    def __init__(self, code):
+        self.code = code
         self.pointer = 0
-        self.OP_DICT = {
-            1: self.intcode_add,
-            2: self.intcode_mult,
-            3: self.intcode_get_input,
-            4: self.intcode_get_output,
-            88: self.intcode_diagnostic,
-            99: self.intcode_quit
-        }
 
-    def intcode_diagnostic(self, args):
-        STEP_SIZE = 1
-        if args == None:
-            args = []
-        while len(args) < 3:
-            args.append(0)
-        print(args)
-        self.pointer += STEP_SIZE
-        return 1
-
-    def intcode_add(self, args=[0]):
-        """
-        moves pointer 4 positions forward, adds positions 1 and 2,
-        outputs position 3
-        """
-        STEP_SIZE = 4
-        if args == None:
-            args = []
-        while len(args) < STEP_SIZE-1:
-            args.append(0)
-
-        # input is the value at position 1 and 2
-        # this is the behavior with arg=1
-        in1 = int(self.code[self.pointer+1])
-        in2 = int(self.code[self.pointer+2])
-        # arg == 0
-        # input is the value at the location specified
-        if not args[0] == 1:
-            in1 = int(self.code[in1])
-        if not args[1] == 1:
-            in2 = int(self.code[in2])
-        result = in1 + in2
-        if not args[2] == 1:
-            out1 = int(self.code[self.pointer+2])
-            self.code[out1] = str(result)
-        else:
-            self.code[self.pointer+2] = str(result)
-        self.pointer += STEP_SIZE
-        return 1
-
-    def intcode_mult(self, args=[0]):
-        """
-        moves pointer 4 positions forward, multiplies positions 1 and 2,
-        outputs position 3
-        """
-        STEP_SIZE = 4
-        if args == None:
-            args = []
-        while len(args) < STEP_SIZE-1:
-            args.append(0)
-        in1 = int(self.code[self.pointer+1])
-        in2 = int(self.code[self.pointer+2])
-        if not args[0] == 1:
-            in1 = int(self.code[in1])
-        if not args[1] == 1:
-            in2 = int(self.code[in2])
-        result = in1 * in2
-        if not args[2] == 1:
-            out1 = int(self.code[self.pointer+2])
-            self.code[out1] = str(result)
-        else:
-            self.code[self.pointer+2] = str(result)
-        self.pointer += STEP_SIZE
-        return 1
-
-    def intcode_get_input(self, args=[0]):
-        STEP_SIZE = 2
-        if args == None:
-            args = []
-        while len(args) < STEP_SIZE-1:
-            args.append(0)
-        status = 1
-        # loc is value of code at position pointer
-        loc = int(self.code[self.pointer+1])
-        # args == 1
-        if args[0] == 1:
-            # location is pointer + 1
-            loc = self.pointer+1
-        in1 = self.input_stream.next()
-        self.code[loc] = in1
-        self.pointer += STEP_SIZE
-        return status
-
-    def intcode_get_output(self, args=[0]):
-        STEP_SIZE = 2
-        if args == None:
-            args = []
-        while len(args) < STEP_SIZE-1:
-            args.append(0)
-        status = 1
-        loc = int(self.code[self.pointer+1])
-        if args[0] == 1:
-            loc = self.pointer+1
-        print(self.pointer, loc)
-        self.output_stream(self.code[loc])
-        self.diagCode = self.code[loc]
-        self.pointer += STEP_SIZE
-        return status
-
-    def intcode_quit(self, args):
+    def terminate(self):
         return 0
 
-    def run_step(self):
-        """
-        perform one operation,
-        """
-        instruction_str = self.code[self.pointer]
-        opcode = instruction_str[-2:]
-        # define args here
-        # default
-        argstring = instruction_str[:-2]
-        arglist = list(argstring)
-        args = [int(x) for x in arglist]
-        args.reverse()
-        try:
-            status = self.OP_DICT[int(opcode)](args)
-        except KeyError:
-            return self.pointer, self.code[self.pointer]
-        return status
-
     def run_code(self):
-        """
-        run through intcode, if no code is provided use the code provided when class was created
-        returns 0 if 99 was reached, 2 if some unexpected op-code encountered
-        """
-        retVal = 1
-        while(retVal == 1):
-            retVal = self.run_step()
+        status = 1
+        while status == 1:
+            status = self.intcode_operation()
+
+    def intcode_operation(self):
+        """ handle the operation """
+        opcode, arglist = self.intcode_blk_parse()
+        retVal = self.OP_KEY[int(opcode)][0]()
+        self.pointer += self.OP_KEY[int(opcode)][1]
         return retVal
 
-    def run_code_d2(self):
-        """
-        run through intcode, if no code is provided use the code provided when class was created
-        returns 0 if 99 was reached, 2 if some unexpected op-code encountered
-        """
-        retVal = 1
-        try:
-            while(retVal == 1):
-                retVal = self.run_step()
-        except IndexError:
-            return(self.pointer, self.code[self.pointer])
-        return retVal
-    def run_diag(self):
-        retVal = 1
-        self.diagCode = '0'
-        while((retVal == 1) and (self.diagCode == '0')):
-            retVal = self.run_step()
-        print("Current Position: ", self.pointer)
+    def debug(self, args):
+        print("opcode: {}".format(self.code[self.pointer]))
+        print("Param 1: {}, Arg: {}".format(self.code[self.pointer+1], args[0]))
+        print("Param 2: {}, Arg: {}".format(self.code[self.pointer+2], args[1]))
+        print("Param 3: {}, Arg: {}".format(self.code[self.pointer+3], args[2]))
+        return 1
 
-    def get_code(self):
-        return self.code
+    def intcode_blk_parse(self):
+        """ get opcode and arglist from current memory location """
+        cur_point = self.pointer
+        raw_opcode = self.code[cur_point]
+        opcode = int(raw_opcode[-2:])
+        raw_arglist = raw_opcode[:-2]
+        arglist = [int(x) for x in raw_arglist]
+        if arglist == None:
+            arglist = [0]
+        arglist.reverse()
+        while len(arglist) < 3:
+            arglist.append(0)
+
+        return opcode, arglist
 
 
 if __name__ == "__main__":
-    testCase2 = int_computer(
-        ["1", '9', '10', '3', '2', '3', '11', '0', '99', '30', '40', '50'])
-    testCase1 = int_computer(["1", "1", "1", "1", "99"])
-    testCase3 = int_computer(["3", "1", "4", "1", "99"], fixed_input('9'))
-    testCase4 = int_computer(['11188', '1188', '188', '88', '1088',
-                              '10088', '10188', '99'])
-    print(testCase4.get_code())
-    print(testCase4.run_code())
-    print(testCase4.get_code())
-    #testCase.run_step()
+    def test_opcode_parser():
+        opcode_parse_tests1 = ['88', '188', '1088', '1188', '10088',
+                               '10188', '11088', '11188', '99']
+        opcode_parse_tests2 = ['8', '108', '1008', '1108', '10008',
+                               '10108', '11008', '11108', '99']
+        tester = int_computer(opcode_parse_tests2)
+        print(tester.code)
+        tester.run_code()
+        print(tester.code)
+
+    test_opcode_parser()
+
+
+    
